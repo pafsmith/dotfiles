82d99196fafc57776813231d4c4f1a5b45f67ba5
Paul Smith <paulalanfsmith@gmail.com>
2025-08-25 19:49:19 +0100

    Initial commit of Neovim config

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "ts_ls", "gopls", "pylsp"}
+        })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+
+} 

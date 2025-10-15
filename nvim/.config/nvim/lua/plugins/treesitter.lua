b6704886a44ffdff2ef3a2faafbac362a2caff5d
Paul Smith <psmith@magna.co.uk>
2025-10-15 15:21:37 +0100

    base

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        ensure_installed = {
+          "bash",
+          "ruby",
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "json",
+          "lua",
+          "python",
+          "go",
+        },
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end
+  }
+}
